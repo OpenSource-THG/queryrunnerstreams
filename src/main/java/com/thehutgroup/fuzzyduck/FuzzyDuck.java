cda4bc1cabb387cf43cdfa895ab6021f49c98467
Dimitar H Petrov <dimitarh.petrov@thehutgroup.com>
2017-05-18 10:37:45 +0100

    remove jenkinsfile and minor refactor

--- src/main/java/com/thehutgroup/fuzzyduck/FuzzyDuck.java
+++ src/main/java/com/thehutgroup/fuzzyduck/FuzzyDuck.java
@@ -1,10 +1,11 @@
 package com.thehutgroup.fuzzyduck;
 
 /**
- * Entrypoint for FuzzyDuckApplicaiton
+ * FuzzyDuck HelloWorld
  */
 public class FuzzyDuckApplication {
 
-  public static void main(String[] args) {
+  public String helloWorld() {
+    return "Hello World";
   }
 }
--- src/test/java/com/thehutgroup/fuzzyduck/test/FuzzyDuckTest.java
+++ src/test/java/com/thehutgroup/fuzzyduck/test/FuzzyDuckTest.java
@@ -3,7 +3,7 @@ package com.thehutgroup.fuzzyduck.test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-public class FuzzyDuckApplicationTest {
+public class FuzzyDuckTest {
 
   @Test
   public void test() {
