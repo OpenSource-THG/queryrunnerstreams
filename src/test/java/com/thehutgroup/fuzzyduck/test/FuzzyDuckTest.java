1f48e9b08beee98cde80695d2f0058db59bf0260
Anthony Stewart <anthony.stewart@thehutgroup.com>
2018-12-04 16:05:23 +0000

    Updating tests to junit5

--- src/test/java/com/thehutgroup/fuzzyduck/test/FuzzyDuckTest.java
+++ src/test/java/com/thehutgroup/fuzzyduck/test/FuzzyDuckTest.java
@@ -1,12 +1,16 @@
 package com.thehutgroup.fuzzyduck.test;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+import org.junit.jupiter.api.Test;
 
 public class FuzzyDuckTest {
 
   @Test
   public void test() {
+    assertThat(true, is(not(false)));
   }
 
 }
