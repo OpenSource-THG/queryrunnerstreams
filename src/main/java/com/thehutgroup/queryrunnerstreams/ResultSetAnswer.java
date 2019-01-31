a5f86b72e8f23c57658c5edb5bd4cac332da80c0
Anthony Stewart <anthony.stewart@thehutgroup.com>
2019-01-31 16:14:52 +0000

    Adding methods for result set testing

---
+++ src/main/java/com/thehutgroup/queryrunnerstreams/ResultSetAnswer.java
@@ -0,0 +1,121 @@
+package com.thehutgroup.queryrunnerstreams;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import org.apache.commons.dbutils.ResultSetHandler;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.mockito.stubbing.Stubber;
+
+public class ResultSetAnswer<T> implements Answer<T> {
+
+  private final ResultSet rs;
+
+  private ResultSetAnswer(ResultSet rs) {
+    this.rs = rs;
+  }
+
+  private ResultSetAnswer(final String[] columnNames, final Object[][] data) {
+    this.rs = new MockResultSet(columnNames, data).build();
+  }
+
+  @Override
+  public T answer(InvocationOnMock invocation) throws Throwable {
+    return Stream.of(invocation.getArguments())
+        .filter(object -> object instanceof ResultSetHandler)
+        .map(object -> (ResultSetHandler<T>) object)
+        .findFirst()
+        .orElseThrow(() -> new Exception("No argument of type ResultSetHandler was passed"))
+        .handle(rs);
+  }
+
+  public static ResultSet mockResultSet(final String[] columnNames, final Object[][] data) {
+    return new MockResultSet(columnNames, data).build();
+  }
+
+  public static <T> Answer<T> withMockResultSet(ResultSet rs) {
+    return new ResultSetAnswer<>(rs);
+  }
+
+  public static Stubber doMockResultSet(ResultSet rs) {
+    return doAnswer(new ResultSetAnswer(rs));
+  }
+
+  public static Stubber doMockResultSet(final String[] columnNames, final Object[][] data) {
+    return doAnswer(new ResultSetAnswer(columnNames, data));
+  }
+
+  private static class MockResultSet {
+
+    private final Map<String, Integer> columnIndices;
+    private final Object[][] data;
+    private int rowIndex;
+
+    private MockResultSet(final String[] columnNames, final Object[][] data) {
+      this.columnIndices = IntStream.range(0, columnNames.length).boxed()
+          .collect(Collectors.toMap(k -> columnNames[k], Function.identity()));
+      this.data = data;
+      this.rowIndex = -1;
+    }
+
+    private ResultSet build() {
+      try {
+        final ResultSet rs = mock(ResultSet.class);
+
+        // Mock rs.next()
+        doAnswer(invocation -> ++rowIndex < data.length).when(rs).next();
+
+        // Mock rs.getObject, getString etc
+        doAnswer(this::getValue).when(rs).getObject(any());
+        doAnswer(getAnswer(String.class)).when(rs).getString(any());
+        doAnswer(getAnswer(Integer.class)).when(rs).getInt(any());
+        doAnswer(getAnswer(Boolean.class)).when(rs).getBoolean(any());
+        doAnswer(getAnswer(Timestamp.class)).when(rs).getTimestamp(any());
+        doAnswer(getAnswer(BigDecimal.class)).when(rs).getBigDecimal(any());
+
+        // Mock rs.getMetaData()
+        final ResultSetMetaData metadata = mock(ResultSetMetaData.class);
+        doReturn(columnIndices.size()).when(metadata).getColumnCount();
+        doReturn(metadata).when(rs).getMetaData();
+
+        return rs;
+      } catch (SQLException ex) {
+        throw new RuntimeException(ex);
+      }
+    }
+
+    private <R> Answer getAnswer(Class<R> clazz) {
+      return invocation -> (R) getValue(invocation);
+    }
+
+    private Object getValue(InvocationOnMock invocation) {
+      Object arg = invocation.getArgument(0);
+      if (arg instanceof String) {
+        return getValue((String) arg);
+      } else {
+        return getValue((int) arg);
+      }
+    }
+
+    private Object getValue(String columnName) {
+      return getValue(columnIndices.get(columnName) + 1);
+    }
+
+    private Object getValue(int columnIndex) {
+      return data[rowIndex][columnIndex - 1];
+    }
+  }
+}
---
+++ src/test/java/com/thehutgroup/queryrunnerstreams/test/ResultSetAnswerTest.java
@@ -0,0 +1,116 @@
+package com.thehutgroup.queryrunnerstreams.test;
+
+import static com.thehutgroup.queryrunnerstreams.ResultSetAnswer.doMockResultSet;
+import static com.thehutgroup.queryrunnerstreams.ResultSetAnswer.mockResultSet;
+import static com.thehutgroup.queryrunnerstreams.ResultSetAnswer.withMockResultSet;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.thehutgroup.queryrunnerstreams.QueryStream;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.apache.commons.dbutils.QueryRunner;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ResultSetAnswerTest {
+
+  private static final String SQL_QUERY = "SELECT * FROM table WHERE value = ?";
+
+  @Test
+  @DisplayName("Test basic mockResultSet()")
+  void mockResultSetTest() throws SQLException {
+
+    final ResultSet rs = mockResultSet(
+        new String[] { "name", "age" }, //columns
+        new Object[][] {
+            { "Alice", 20 },
+            { "Bob", 35 },
+            { "Charles", 50 }
+        });
+
+    assertThat(rs.next(), is(true));
+    assertThat(rs.getString("name"), is("Alice"));
+    assertThat(rs.getInt("age"), is(20));
+
+    assertThat(rs.next(), is(true));
+    assertThat(rs.getString("name"), is("Bob"));
+    assertThat(rs.getInt("age"), is(35));
+
+    assertThat(rs.next(), is(true));
+    assertThat(rs.getString("name"), is("Charles"));
+    assertThat(rs.getInt("age"), is(50));
+
+    assertThat(rs.next(), is(false));
+
+  }
+
+  @Test
+  @DisplayName("Test using doMockResultSet().when()")
+  void doMockResultSetTest() throws SQLException {
+
+    QueryRunner queryRunner = mock(QueryRunner.class);
+
+    doMockResultSet(new String[] { "name", "age" }, //columns
+        new Object[][] {
+            { "Alice", 20 },
+            { "Bob", 35 },
+            { "Charles", 50 }
+        })
+        .when(queryRunner)
+        .query(eq(SQL_QUERY), any(), anyInt());
+
+    List<String> names = queryRunner.query(
+        SQL_QUERY,
+        rs -> QueryStream.of(rs)
+            .map(row -> row.get("name", String.class))
+            .collect(Collectors.toList()),
+        1);
+
+    assertThat(names.size(), is(3));
+    assertThat(names.get(0), is("Alice"));
+    assertThat(names.get(1), is("Bob"));
+    assertThat(names.get(2), is("Charles"));
+
+  }
+
+  @Test
+  @Disabled
+  @DisplayName("Test using when().thenAnswer(withMockResultSet())")
+  void whenDoAnswerWithMockResultSetTest() throws SQLException {
+
+    QueryRunner queryRunner = mock(QueryRunner.class);
+
+    final ResultSet rs = mockResultSet(
+        new String[] { "name", "age" }, //columns
+        new Object[][] {
+            { "Alice", 20 },
+            { "Bob", 35 },
+            { "Charles", 50 }
+        });
+
+    when(queryRunner.query(eq(SQL_QUERY), any(), anyInt())).thenAnswer(withMockResultSet(rs));
+
+    List<String> names = queryRunner.query(
+        SQL_QUERY,
+        resultSet -> QueryStream.of(resultSet)
+            .map(row -> row.get("name", String.class))
+            .collect(Collectors.toList()),
+        1);
+
+    assertThat(names.size(), is(3));
+    assertThat(names.get(0), is("Alice"));
+    assertThat(names.get(1), is("Bob"));
+    assertThat(names.get(2), is("Charles"));
+
+  }
+
+}
