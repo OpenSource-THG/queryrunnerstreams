b250d3efe94b8cdc03eef32ed85411dd02ab098e
Anthony Stewart <anthony.stewart@thehutgroup.com>
2019-09-18 15:47:19 +0100

    Updating NamedParameterQueryRunner so that you can pass in a Collection

--- src/main/java/com/thehutgroup/queryrunnerstreams/NamedParameterParser.java
+++ src/main/java/com/thehutgroup/queryrunnerstreams/NamedParameterParser.java
@@ -2,6 +2,8 @@ package com.thehutgroup.queryrunnerstreams;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -9,24 +11,42 @@ import java.util.regex.Pattern;
 
 class NamedParameterParser {
 
-  private static final Pattern pattern = Pattern.compile(":[a-zA-Z0-9\\-]+");
+  private static final Pattern pattern = Pattern.compile("(:[a-zA-Z0-9\\-]+)(\\[\\])?");
 
-  SqlAndParamsList parseNamedParameters(String sql, Map<String, Object> params)
+  SqlAndParamsList parseNamedParameters(final String sql, final Map<String, Object> params)
       throws SQLException {
 
+    StringBuffer resultSql = new StringBuffer();
     List<Object> paramList = new ArrayList<>();
 
     Matcher matcher = pattern.matcher(sql);
     while (matcher.find()) {
-      String key = matcher.group().substring(1);
+      String key = matcher.group(1).substring(1);
+      boolean batch = matcher.group(2) != null;
       Object value = params.get(key);
       if (value == null) {
         throw new SQLException("Parameter :" + key + " could not be matched");
       }
-      paramList.add(value);
+
+      if (batch) {
+        if (!(value instanceof Collection<?>)) {
+          throw new SQLException("Value of :" + key + " is not a collection");
+        }
+        int count = 0;
+        for (Object subValue : ((Collection) value)) {
+          count++;
+          paramList.add(subValue);
+        }
+        matcher.appendReplacement(resultSql, String.join(", ", Collections.nCopies(count, "?")));
+      } else {
+        matcher.appendReplacement(resultSql, "?");
+        paramList.add(value);
+      }
     }
 
-    return new SqlAndParamsList(matcher.replaceAll("?"), paramList.toArray());
+    matcher.appendTail(resultSql);
+
+    return new SqlAndParamsList(resultSql.toString(), paramList.toArray());
   }
 
   static class SqlAndParamsList {
